4b2ae5affc8365f57729d76905c49d1f510fe2b2
Vladimir Vetrov <southveter@gmail.com>
2017-09-06 18:59:38 +0300

    HW4: File example recursion

---
+++ src/main/java/examples/filesystem/Files.java
@@ -0,0 +1,49 @@
+package examples.filesystem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Files {
+    public static void main(String[] args) {
+        String filePath = "./src/main/java/Loader.java";
+
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File("./src");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : list) {
+                System.out.println(name);
+            }
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        printDirectoryDeeply(dir, "");
+    }
+    // TODO: make pretty output
+    public static void printDirectoryDeeply(File dir, String offset) {
+        File[] files = dir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(offset + "F: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println(offset + "D: " + file.getName());
+                    printDirectoryDeeply(file, offset + "  ");
+                }
+            }
+        }
+    }
+}
