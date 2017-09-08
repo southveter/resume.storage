a4979ad8bf36254cb20d9e064ef7f39506cbc9a4
Vladimir Vetrov <southveter@gmail.com>
2017-09-08 18:53:41 +0300

    HW6: JSON serializer implementation

--- src/test/java/model/AllStorageTest.java
+++ src/test/java/model/AllStorageTest.java
@@ -13,7 +13,8 @@ import org.junit.runners.Suite;
         ObjectStreamStorageTest.class,
         FileStorageTest.class,
         PathStorageTest.class,
-        XmlPathStorageTest.class
+        XmlPathStorageTest.class,
+        JsonPathStorageTest.class
 })
 public class AllStorageTest {
 
---
+++ src/test/java/model/JsonPathStorageTest.java
@@ -0,0 +1,11 @@
+package model;
+
+import storage.PathStorage;
+import storage.serializer.JsonStreamSerializer;
+
+public class JsonPathStorageTest extends AbstractStorageTest {
+
+    public JsonPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new JsonStreamSerializer()));
+    }
+}
